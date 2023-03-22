ffa205d5e71c365ae4d79817ec335035a60cfd8b
Maksim Polyakov <mpolakov777@gmail.com>
2023-03-22 23:50:45 +0100

    added header and first section

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('.location-inp').on('click', function() {
+        $('.select-first').toggleClass('active');
+    })
+    $('.location-inp-second').on('click', function() {
+        $('.select-second').toggleClass('active');
+    })
+})
