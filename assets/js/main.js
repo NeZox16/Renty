88a7eb35030e4a615585f2bdd751f8bd757c272a
Maksim Polyakov <mpolakov777@gmail.com>
2023-03-25 18:41:39 +0100

    "added fifth section"

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,4 +5,18 @@ $(document).ready(function() {
     $('.location-inp-second').on('click', function() {
         $('.select-second').toggleClass('active');
     })
+
+    $('.new-wrapper').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        slidesPerRow: 1,
+        variableWidth: true,
+        infinite: false,
+        centerPadding: '0px',
+        cssEase: 'linear',
+        focusOnSelect: true,
+        prevArrow:"<button type='button' class='slick-prev'></button>",
+        nextArrow:"<button type='button' class='slick-next'></button>",
+      });
+
 })
